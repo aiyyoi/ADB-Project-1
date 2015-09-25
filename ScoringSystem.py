667cd4ffe6349617401833fe4e52fa10af27000a
Akshaan Kakar <akshaan.crackers@gmail.com>
2015-09-24 23:51:23 -0400

    Added Preprocessor class with a stop-word removal method and a skeleton file for ScoringSystem

---
+++ ScoringSystem.py
@@ -0,0 +1,16 @@
+# Input:
+#	Document titles
+#	Document descriptions
+#	Document links
+#	Document text
+#	Relevance feedback
+#
+# Output:
+#	One or Two terms to be added to query for next iteration
+
+class ScoringSystem():
+
+	"""Method to calculate score from documents and relevance feedback"""
+	@classmethod	
+	def score(link,title,desc):
+		pass		
